9e38fc327e44ee70b368c8d8bf2dff5f4f4945f2
Bo Zhao <bozhao12@gmail.com>
2021-01-06 16:03:37 -0500

    Adding proficiencies with just text

--- src/Resume/en/index.jsx
+++ src/Resume/en/index.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 
 import Header from './header'
+import Proficiencies from './proficiencies'
 
 export default class EnglishResume extends Component {
   render() {
     return (
       <div>
         <Header />
+        <Proficiencies />
       </div>
     );
   }
---
+++ src/Resume/en/proficiencies.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import './proficiencies.css';
+import _ from "lodash";
+
+function generateProficienciesLine(entry) {
+  const title = entry['title'];
+  const description = entry['description'];
+  return (
+    <div class="pro-row">
+      <div class="pro-title">{ title }</div>
+      <div>{ description }</div>
+    </div>
+  )
+}
+
+function generateProficiencies() {
+  const proficiencies = [
+    {'title': 'Python', 'description': 'Extensive experience coding in Python, utilizing best practices for maximum readability and minimal sadness.'},
+    {'title': 'Kubernetes', 'description': 'Setting up clusters, designing tools for secure production environments, high level of understanding with k8s natives.'},
+    {'title': 'Containers', 'description': 'Docker, cri-o. Can’t do Kubernetes without containers.'},
+    {'title': 'AWS', 'description': 'Extensive usage of technologies such as EC2, ECS, S3, DynamoDB, IAM, and the list goes on...'},
+    {'title': 'Config Management', 'description': 'Terraform, Packer, SaltStack. Automating generations of machines configurations and cloud resources.'},
+    {'title': 'DevOps', 'description': 'Everyday devops things, deep diving into new codebases during incidents, hacking up scripts for fast fixes, being on linux, and keeping the gears turning day to day.'},
+    {'title': 'Tooling', 'description': 'Continuous integration, developer tooling, bash scripting, etc. Empower other devs to be more productive.'},
+    {'title': 'More?', 'description': 'Anything that isn’t already in there? That’s okay because I am a quick learner and can get settled in very quickly in a new environment.'},
+  ];
+  return proficiencies.map(generateProficienciesLine);
+
+}
+
+
+export default class Proficiencies extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Proficiencies</h3>
+        { generateProficiencies() }
+      </div>
+    );
+  }
+}
