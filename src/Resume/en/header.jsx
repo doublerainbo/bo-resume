816c0654b1d649ce1755e7c4f342cd13243fdb8a
Bo Zhao <bozhao12@gmail.com>
2021-01-09 20:47:57 -0500

    get the page to look normalish in one page on print

--- src/Resume/en/header.jsx
+++ src/Resume/en/header.jsx
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
+import './header.css'
+
+function swapLanguage() {
+  // this.renderEnglish = false;
+}
 
 export default class Header extends Component {
   render() {
     return (
       <div>
-        <div className="resume-body">
-          <h1>Bo Zhao</h1>
-          <button className="site-navigation">Chinese</button>
+        <div className="header">
+          <h1 className="my-name">Bo Zhao</h1>
+          <button className="site-navigation" onClick={swapLanguage}>中文</button>
         </div>
-        <h3>Software Engineer</h3>
         <hr/>
+        <h4 className="my-title">Software Engineer</h4>
       </div>
     );
   }
