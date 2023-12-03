d1561636723046277be7a3af4808f82820adb75c
Bo Zhao <bozhao12@gmail.com>
2023-12-03 15:02:13 -0500

    2023 winter update (#11)
    * add google stuff
    
    * more content

--- src/Resume/en/index.jsx
+++ src/Resume/en/index.jsx
@@ -5,14 +5,14 @@ const en = {
   'lang_value': 'ch',
   'proficiencies_title': 'Technical Proficiencies',
   'proficiencies': [
-    {'title': 'Python', 'description': 'Extensive experience coding in Python. Utilizes best practices for maximum readability and minimal sadness.'},
-    {'title': 'Kubernetes', 'description': 'Cluster setup with or without cloud offerings. Tooling for secure production environments. High level of understanding with k8s natives.'},
+    {'title': 'Python', 'description': 'Extensive experience coding in Python. Utilizes best practices for maximum readability.'},
+    {'title': 'AWS & GCP', 'description': 'Cloud management in both AWS and GCP with its various offerings.'},
+    {'title': 'Kubernetes', 'description': 'High level of understanding with k8s natives and operational expertise.'},
     {'title': 'Containers', 'description': 'Docker, cri-o. Hands-on experience at the API layer and the OS level natives.'},
-    {'title': 'AWS', 'description': 'Extensive usage of AWS technologies such as EC2, ECS, S3, DynamoDB, IAM, the list goes on...'},
-    {'title': 'Config Management', 'description': 'Terraform, Packer, SaltStack. Been automating the setup of different types of machines configurations and cloud resources since I was 8 (not actually).'},
+    {'title': 'Config Management', 'description': 'Terraform, Packer, SaltStack.'},
     {'title': 'DevOps', 'description': 'Everyday DevOps things, triaging the unknown during incidents, hacking up scripts for fast fixes, and keeping the gears turning day to day.'},
     {'title': 'Tooling', 'description': 'CI/CD, developer tooling, bash scripting, etc. Empower other devs to be more productive.'},
-    {'title': 'And...', 'description': 'Also worked with technologies such as Go, Jenkins, Java, GCP, React, etc in the past. If this list needs expansion, I can quickly learn and adapt in a new environment.'},
+    {'title': 'And...', 'description': 'Also worked with technologies such as Go, Java, Jenkins, React, etc. If this list needs expansion, I can quickly learn and adapt in a new environment.'},
   ],
   'degree': 'Bachelor of Applied Science',
   'program': 'Computer Engineering',
@@ -22,7 +22,6 @@ const en = {
   'hobby_title': 'Hobby Projects:',
   'hobby_desc': 'I enjoy making some projects in my downtime, such as:',
   'hobbies': [
-    {'title': 'Sleep Tracking Hat', 'desc': 'Track sleep patterns by combining an EEG to measure brainwaves and microphone for audio feedback.'},
     {'title': 'Music Controlled Lights', 'desc': 'Utilizing an Arduino to control RBG light patterns using sound as input data.'},
     {'title': 'IoT Synced Reminder Buttons', 'desc': 'A button that lights up for reminders that triggers at multiple locations and the state is synced across the devices'},
     {'title': 'Hungry and Indecisive?', 'desc': 'Find some food near you:', 'url': 'nooyoupick.com'}
@@ -31,17 +30,18 @@ const en = {
   'experiences': [
     {
       'title': 'Software Engineer',
-      'company': 'Sidewalk Labs',
+      'company': 'Google / Sidewalk Labs',
       'location': 'New York, NY, USA',
       'timeline': 'Jun 2021 - Present',
       'points': [
         'Contributed in multiple facets of the Pebble parking sensors product, including firmware, backend and frontend software, and software infrastructure',
+        'Drove the migration of the backend stack from GCP to internal Google infrastructure',
         'Lead the effort in setting up the software oncall rotation with actionable alarms and minimizing engineer producitivity loss',
       ],
     },
     {
       'title': 'Senior DevOps\nEngineer',
-      'company': 'Juul Labs Inc.',
+      'company': 'Juul Labs',
       'location': 'San Francisco, CA, USA',
       'timeline': 'Oct 2019 - Oct 2020',
       'points': [
@@ -52,7 +52,7 @@ const en = {
     },
     {
       'title': 'Software Engineer',
-      'company': 'Lyft Inc.',
+      'company': 'Lyft',
       'location': 'San Francisco, CA, USA',
       'timeline': 'Jul 2016 - Sep 2019',
       'points': [
